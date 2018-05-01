2b6f93978d22470c22b4ba4f513c6431a8c38789
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-01 02:00:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include? brand == false
-      BRANDS << brand
+    BRANDS.push(brand) unless BRANDS.include?(brand)
     end
   end
 
